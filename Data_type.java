299544724f2ee8abb83a2815771880a22626c714
Emdadul Islam <58410798+emdadulislam580@users.noreply.github.com>
2021-03-14 11:30:15 +0600

    Data type

---
+++ Data_type.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) {
+      short shortnum=9;
+      int secondint=20;
+      double result = secondint/3.0;
+      char ch = 'a';
+      boolean statement = false;
+      System.out.println(shortnum);
+      System.out.println(result);
+      System.out.println(statement);
+    }
+}
